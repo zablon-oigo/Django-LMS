344f8501b01e974d0cfb46acba0631e06a802808
zablon <zablonoigo51@gmail.com>
2024-02-18 14:23:28 +0300

    feat: view to order module content

--- courses/views.py
+++ courses/views.py
@@ -159,3 +159,11 @@ class ModuleOrderView(CsrfExemptMixin,JSONRequestResponseMixin, View):
 
         return self.render_json_response({'saved':'OK'})
     
+class ContentView(CsrfExemptMixin,JSONRequestResponseMixin,View):
+
+    def post(self, request):
+        for id,order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+
+            return self.render_json_response({'saved':'OK'})
+        
