2cc2650d268e76c4bfe2b04412f5074c9ed2656d
zablon <zablonoigo51@gmail.com>
2024-02-19 11:54:46 +0300

    fix: delete slug

--- courses/views.py
+++ courses/views.py
@@ -24,7 +24,7 @@ class OwnerEditMixin:
 
 class OwnerCourseMixin(OwnerMixin,LoginRequiredMixin,PermissionRequiredMixin):
     model =Course
-    fields=['subject','title','slug','overview']
+    fields=['subject','title','overview']
 
     success_url=reverse_lazy('manage_course_list')
 
