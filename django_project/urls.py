360c9052a4a476b55663f4a5f944d7250a701244
zablon <zablonoigo51@gmail.com>
2024-02-20 13:31:39 +0300

    fix: rename the app

--- django_project/urls.py
+++ django_project/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
-    path('students/', include('student.urls')),
+    path('students/', include('students.urls')),
 ]
 
 
