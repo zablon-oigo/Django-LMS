8d3bc6134e36ba2b4c5e2e22bf427ab867668a90
zablon <zablonoigo51@gmail.com>
2024-02-20 13:44:34 +0300

    feat: student enroll course view

--- students/views.py
+++ students/views.py
@@ -3,6 +3,9 @@ from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.views.generic.edit import CreateView
+from .forms import CourseEnrollForm
+from django.views.generic.edit import FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
@@ -20,3 +23,16 @@ class StudentRegistrationview(CreateView):
         login(self.request, user)
 
         return result
+
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course=None
+    form_class=CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course =form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail', args=[self.course.id])
