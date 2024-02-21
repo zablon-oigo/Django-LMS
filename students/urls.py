542f19031879a466892a5c025721b36df01cbdae
zablon <zablonoigo51@gmail.com>
2024-02-21 07:47:08 +0300

    feat: student course lis view and course detail url path

--- students/urls.py
+++ students/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import StudentRegistrationview,StudentEnrollCourseView
+from .views import StudentRegistrationview,StudentEnrollCourseView\
+                        ,StudentCourseListView,StudentCourseDetailView
 
 
 urlpatterns=[
     path('register/',StudentRegistrationview.as_view(), name='student_registration'),
     path('enroll-course/', StudentEnrollCourseView.as_view(),name='student_enroll_course'),
+    path('courses/',StudentCourseListView.as_view(), name='student_course_list'),
+    path('course/<pk>/',StudentCourseDetailView.as_view(), name='student_course_detail'),
+    path('course/<pk>/<module_id>/',StudentCourseDetailView.as_view(), name='student_course_detail_module'),
+
 
 ]
