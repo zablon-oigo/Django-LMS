462b91abccd8c380ae5bee6b305dea06a4f9343c
zablon <zablonoigo51@gmail.com>
2024-02-21 09:45:10 +0300

    feat: django debug toolbar

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS=[
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -40,9 +43,11 @@ INSTALLED_APPS = [
     'courses.apps.CoursesConfig',
     'students.apps.StudentsConfig',
     'embed_video',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
