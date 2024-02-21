18791951a66ecb7d41994e06528eec5e060efa5a
zablon <zablonoigo51@gmail.com>
2024-02-21 10:00:13 +0300

    feat: caching template framework

--- students/urls.py
+++ students/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import StudentRegistrationview,StudentEnrollCourseView\
                         ,StudentCourseListView,StudentCourseDetailView
-
+from django.views.decorators.cache import cache_page
 
 urlpatterns=[
     path('register/',StudentRegistrationview.as_view(), name='student_registration'),
     path('enroll-course/', StudentEnrollCourseView.as_view(),name='student_enroll_course'),
     path('courses/',StudentCourseListView.as_view(), name='student_course_list'),
-    path('course/<pk>/',StudentCourseDetailView.as_view(), name='student_course_detail'),
-    path('course/<pk>/<module_id>/',StudentCourseDetailView.as_view(), name='student_course_detail_module'),
+    path('course/<pk>/',cache_page(60*15) (StudentCourseDetailView.as_view()), name='student_course_detail'),
+    path('course/<pk>/<module_id>/',cache_page(60*15)(StudentCourseDetailView.as_view()), name='student_course_detail_module'),
 
 
 ]
