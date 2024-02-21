5b923086a919f00bb33d10e3d3a1e14ef2c8c1b6
zablon <zablonoigo51@gmail.com>
2024-02-21 07:08:08 +0300

    feat: enroll form url path

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import StudentRegistrationview
+from .views import StudentRegistrationview,StudentEnrollCourseView
 
 
 urlpatterns=[
-    path('register/',StudentRegistrationview.as_view(), name='student_registration')
+    path('register/',StudentRegistrationview.as_view(), name='student_registration'),
+    path('enroll-course/', StudentEnrollCourseView.as_view(),name='student_enroll_course'),
+
 ]
