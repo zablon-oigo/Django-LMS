603dd67caf96edfb153c284b2afbe3cec2ca0bba
zablon <zablonoigo51@gmail.com>
2024-02-22 12:09:36 +0300

    feat: Adding additional actions to viewsets to automatically generate a route

--- courses/api/views.py
+++ courses/api/views.py
@@ -7,11 +7,17 @@ from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
-
+from rest_framework.decorators import action
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset=Course.objects.all()
     serializer_class=CourseSerializer
-    
+    @action(detail=True, methods=['post'], authenticatication_classes=[IsAuthenticated])
+
+    def enroll(self, request, *args, **kwargs):
+        course=self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled':True})
+
 class SubjectListView(generics.ListAPIView):
     queryset=Subject.objects.all()
     serializer_class=SubjectSerializer
