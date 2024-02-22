9cb64bde01c78ee2a0ae2af935cb0400662ee5c8
zablon <zablonoigo51@gmail.com>
2024-02-22 11:26:56 +0300

    feat: Course enroll Custom API

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
-from courses.models import Subject
+from courses.models import Subject,Course
 from courses.api.serializers import SubjectSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class SubjectListView(generics.ListAPIView):
@@ -11,3 +14,12 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset=Subject.objects.all()
     serializer_class=SubjectSerializer
+
+
+
+class CourseEnrollView(APIView):
+    def post(self, request, pk, format=None):
+        course=get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'emrolled':True})
+    
