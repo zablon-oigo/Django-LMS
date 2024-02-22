5956de1ed1a10324fd4e075f79c5045b06beff80
zablon <zablonoigo51@gmail.com>
2024-02-22 12:34:41 +0300

    feat: Serialize course contents

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from courses.models import Subject, Course, Module
+from courses.models import Subject, Course, Module,Content 
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -20,3 +20,16 @@ class CourseSerializer(serializers.ModelSerializer):
         fields=[
             'id','subject','title','slug','overview','created','owner','modules'
         ]
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+    
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item=ItemRelatedField(read_only=True)
+    class Meta:
+        model=Content
+        fields=['order','item']
+        
