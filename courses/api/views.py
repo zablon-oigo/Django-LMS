181ba25e5036d870ea706861e54158f2cbd125d7
zablon <zablonoigo51@gmail.com>
2024-02-23 08:10:10 +0300

    feat: view action to retreive course contents

--- courses/api/views.py
+++ courses/api/views.py
@@ -8,9 +8,20 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import CourseWithContentsSerializer
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset=Course.objects.all()
     serializer_class=CourseSerializer
+    @action(
+            default=True,
+            methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled]
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
     @action(detail=True, methods=['post'], authenticatication_classes=[IsAuthenticated])
 
     def enroll(self, request, *args, **kwargs):
