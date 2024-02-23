00ea3912be0adb90299167c55bf0a9aa1dec153b
zablon <zablonoigo51@gmail.com>
2024-02-23 08:09:24 +0300

    feat:extending Course serializer

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -33,3 +33,16 @@ class ContentSerializer(serializers.ModelSerializer):
         model=Content
         fields=['order','item']
         
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents=ContentSerializer(many=True)
+    class Meta:
+        model=Module
+        fields=['order','title','description','contents']
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules=ModuleWithContentsSerializer(many=True)
+    class Meta:
+        model=Course
+        fields=['id','subject','title','slug','overview','created','owner','modules']
+        
