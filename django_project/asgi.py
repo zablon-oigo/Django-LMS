89af9811070b688b55415c29b372c8372b23dca0
zablon <zablonoigo51@gmail.com>
2024-02-24 06:43:41 +0300

    feat: Adding a new route for the websocket protocol

--- django_project/asgi.py
+++ django_project/asgi.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
-
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 django_asgi_app = get_asgi_application()
@@ -20,5 +21,10 @@ django_asgi_app = get_asgi_application()
 application=ProtocolTypeRouter(
     {
         'http':django_asgi_app,
+        'websocket':AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        )
     }
 )
