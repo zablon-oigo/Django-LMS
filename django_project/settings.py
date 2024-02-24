faf1844ea0558045bf11cc3c515067fb2aeab692
zablon <zablonoigo51@gmail.com>
2024-02-24 06:24:11 +0300

    feat: asgi application settings used by channels to locate the root routing configurations

--- django_project/settings.py
+++ django_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'redisboard',
     'rest_framework',
+    'channels'
 ]
 REST_FRAMEWORK={
     'DEFAULT_PERMISSION_CLASSES':[
@@ -86,6 +87,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
+ASGI_APPLICATION='django_project.routing.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
