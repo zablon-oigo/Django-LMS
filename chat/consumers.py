1971fdb2561b35c2a2d57932c77351914ba63eda
zablon <zablonoigo51@gmail.com>
2024-02-24 11:31:22 +0300

    feat: asynchronous channel layer methods

--- chat/consumers.py
+++ chat/consumers.py
@@ -14,10 +14,24 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
     def receive(self, text_data):
         test_data_json=json.loads(text_data)
         message=test_data_json['message']
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type':'chat_message',
+                'message':message,
+            }
+        )
 
         self.send(text_data=json.dumps({'message':message}))
+
+    
+    def chat_message(self, event):
+        self.send(text_data=json.dumps(event))
