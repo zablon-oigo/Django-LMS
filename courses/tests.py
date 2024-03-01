e9b7be78f4f88336ca8a91b8cb53a377fcbf9bef
zablon <zablonoigo51@gmail.com>
2024-03-01 23:10:57 +0300

    feat: test model

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Course
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class CourseTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user=get_user_model().objects.create_user(
+            username="testuser",
+            password="secret",
+            email="testuser@mail.com"
+        )
+        cls.course=Course.objects.create(
+            owner=cls.user,
+            subject="New subject",
+            title="chapter one",
+            slug="chapter-one",
+            overview="description",
+        )
